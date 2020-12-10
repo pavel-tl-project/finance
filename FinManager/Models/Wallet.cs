b504d56833ba08147163ac62b900050d466ab393
adreii20 <netydoma95@gmail.com>
2020-12-10 12:09:21 +0200

    Изменен класс Wallet

--- FinManager/Models/Wallet.cs
+++ FinManager/Models/Wallet.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace FinManager.Models
 {
-    class Wallet
+    public class Wallet
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public decimal Sum { get; set; }
     }
 }
